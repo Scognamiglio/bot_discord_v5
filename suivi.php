aa6d8d7beaf8c0b106ce98ad7ae74fdafe11ec0c
Scognamiglio Loïc <loic.scognamiglio16@gmail.com>
2021-06-23 21:54:00 +0200

    creation suivi

--- suivi.php
+++ suivi.php
@@ -4,12 +4,11 @@ $suivi = $GLOBALS['suivi'][$id];
 
 if(isset($suivi['create'])){
     $etape = count($suivi['create']);
-    var_dump($etape);
 
     switch ($etape){
         case 0:
             if($message->content == 0){
-                $message->channel->sendMessage("Aller sur cette url : http://51.91.99.243/SDA/");
+                $message->channel->sendMessage("Aller sur cette url : http://51.91.99.243/SDA/index.php?page=new_char");
                 unset($GLOBALS['suivi'][$id]['create']);
             }elseif($message->content == 1){
                 $message->channel->sendMessage("Si vous désirez mettre en pause la création de votre fiche, utilisez la commande !save.\n\nIndiquez-nous votre prénom.");
