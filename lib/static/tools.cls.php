0a2d463085810e47ee616d48a0b26fb8f13ba142
J-Estivals <jeremy.estivals@outlook.com>
2022-06-22 22:35:32 +0200

    fix(alias) refonte du code

--- lib/static/tools.cls.php
+++ lib/static/tools.cls.php
@@ -3,7 +3,9 @@
 /*
 boite a outil 
 */
+$tab;
 class tools
+
 {
     public static function prepareInsert($table,$param) {
 
@@ -32,27 +34,26 @@ class tools
     public static function array_map_assoc(callable $f, array $a) {
     return array_column(array_map($f, array_keys($a), $a), 1, 0);
     }
-
-    public static function alias($act = null)
-    {
-        if (!isset($GLOBALS['tableaualias'])) {
-            $GLOBALS['tableaualias'] = sql::fetchAll("SELECT * from alias");
-        }
-        $tab =  $GLOBALS['tableaualias'] ;
-        //donne la priorité aux commande native sur les alias
-        foreach ($tab as $line){
-            if ($line[0] === $act) {
-               return $line[0];
-            }
-        }
-        //verifiei la présence d'un alias compatible
-        foreach ($tab as $line) {
-            $values = explode(",",$line[1]);
-            foreach ($values as $key)  {
-                if (trim($key) === $act) {
-                    return $line[0];
-                }
+    /**
+     * 
+     */
+    public static function alias($act)
+    {      
+        global $tab;
+        if (empty($tab)) {
+            foreach (sql::fetchAll("SELECT * from alias") as $ligne ) {
+                $tab[$ligne["original"]] = array_map("trim",explode(",",$ligne["autres"])) ;
             }            
-        }            
-    }        
-}
+        } 
+        //trouver si c'est une clé
+        if (in_array($act,array_keys($tab))) {
+            return $act;
+        }    
+        foreach ($tab as $clef => $valeur ) {
+            if (in_array($act,$valeur)) {
+                return $clef;
+            };
+        }        
+         return $act;          
+    }
+}        
